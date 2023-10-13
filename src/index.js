4999f721ba3042a0592d76a12743865c926f8265
malekhizaoui <hizaoui.malek.habib@gmail.com>
2023-10-13 12:49:14 +0200

    remove bcrypt

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import { StreamChat } from "stream-chat";
 import { v4 as uuidv4 } from "uuid";
-import bcrypt from "bcrypt";
 const {Canvas} = require('canvas');
 
 const app = express();
@@ -20,7 +19,7 @@ app.post("/signup", async (req, res) => {
   try {
     const { firstName, lastName, username, password } = req.body;
     const userId = uuidv4();
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = password
     const token = serverClient.createToken(userId);
     res.json({ token, userId, firstName, lastName, username, hashedPassword });
   } catch (error) {
@@ -35,10 +34,8 @@ app.post("/login", async (req, res) => {
     if (users.length === 0) return res.json({ message: "User not found" });
 
     const token = serverClient.createToken(users[0].id);
-    const passwordMatch = await bcrypt.compare(
-      password,
-      users[0].hashedPassword
-    );
+    const passwordMatch = password
+     
 
     if (passwordMatch) {
       res.status(200).json({
