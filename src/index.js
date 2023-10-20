abb5df8e03ac73d5f77b7946140f1b55049875ae
malekhizaoui <hizaoui.malek.habib@gmail.com>
2023-10-20 22:20:41 +0200

    switch package manager from yarn to npm

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { StreamChat } from "stream-chat";
 import { v4 as uuidv4 } from "uuid";
-
+import bcrypt from "bcrypt";
 const app = express();
 
 app.use(cors());
@@ -18,7 +18,7 @@ app.post("/signup", async (req, res) => {
   try {
     const { firstName, lastName, username, password } = req.body;
     const userId = uuidv4();
-    const hashedPassword = password
+    const hashedPassword = await bcrypt.hash(password, 10);
     const token = serverClient.createToken(userId);
     res.json({ token, userId, firstName, lastName, username, hashedPassword });
   } catch (error) {
@@ -33,8 +33,10 @@ app.post("/login", async (req, res) => {
     if (users.length === 0) return res.json({ message: "User not found" });
 
     const token = serverClient.createToken(users[0].id);
-    const passwordMatch = password
-     
+    const passwordMatch = await bcrypt.compare(
+      password,
+      users[0].hashedPassword
+    );
 
     if (passwordMatch) {
       res.status(200).json({
